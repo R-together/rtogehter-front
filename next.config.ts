6c69e56b80a14c1f7d6f0c158e5b78a68380fd61
Takahashi, Minato | Takamy | ECMPD <minato.a.takahashi@rakuten.com>
2025-02-14 23:08:43 +0900

    🚚 Chore: add nextConfig so that pr-preview works correctly

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
+const isPreview = process.env.IS_PREVIEW === "true";
+
+/** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  // Configure basePath and assetPrefix to support preview deployments
+  basePath: isPreview ? `/pr-preview-${process.env.PR_NUMBER}` : "",
+  assetPrefix: isPreview ? `/pr-preview-${process.env.PR_NUMBER}/` : "",
 };
 
 export default nextConfig;
