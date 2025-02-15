3e218c82407eaf528c72b18eaf07a88e835793f9
Takahashi, Minato | Takamy | ECMPD <minato.a.takahashi@rakuten.com>
2025-02-16 00:24:11 +0900

    ✨ Feat: create a new page for component checking (it'll be deleted once all components are implemented)

---
+++ app/components-check/page.tsx
@@ -0,0 +1,35 @@
+/* 
+    This is a page to preview all the components in the app.
+    It should be deleted before the app is deployed.
+*/
+
+import { BasicForm } from "@/app/__components/BasicForm";
+import { BasicButton } from "../__components/BasicButton";
+
+export default function ComponentsCheckPage() {
+  return (
+    <div className="flex flex-col gap-2 p-3 ">
+      <h1 className="font-bold text-3xl">Components Check Page</h1>
+      <div>
+        <h2 className="font-bold">Basic Form</h2>
+        <h3>With Label</h3>
+        <BasicForm id="basic-form-with-label" labelName="Basic Form" />
+        <h3>Without Label</h3>
+        <BasicForm id="basic-form-without-label" labeled={false} />
+        <h3>For password</h3>
+        <BasicForm
+          id="basic-form-for-password"
+          labelName="For password"
+          type="password"
+        />
+      </div>
+      <div>
+        <h2 className="font-bold">Basic Button</h2>
+        <h3>Light theme</h3>
+        <BasicButton theme="light" buttonLabel="Basic Button" />
+        <h3>Dark theme</h3>
+        <BasicButton theme="dark" buttonLabel="Basic Button" />
+      </div>
+    </div>
+  );
+}
